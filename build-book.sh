23a9c5d8fa63229a705e6a258e045474f2ef7cdb
yukun <yukun@xsky.com>
2019-08-08 14:35:01 +0800

    update build

--- build-book.sh
+++ build-book.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
+node server/download.js
 cd ./online-help-cn
 gitbook install
-cd ../online-help-en
-gitbook install
-cd ../
-node server/download.js
-cd online-help-cn
 gitbook build ./ ../dist/_book_zh-cn
 cd ../online-help-en
+gitbook install
 gitbook build ./ ../dist/_book_en
 cd ../
 tar -czvf dist.tar.gz dist
--- build.sh
+++ build.sh
@@ -3,4 +3,8 @@
 rm -r node_modules
 yarn global add gitbook-cli
 yarn install
-yarn global add phantomjs-prebuilt
+RUN apk update && apk add --no-cache wget && apk --no-cache add openssl wget && apk add ca-certificates && update-ca-certificates
+RUN wget -qO- "https://github.com/dustinblackman/phantomized/releases/download/2.1.1a/dockerized-phantomjs.tar.gz" | tar xz -C / \
+    && npm config set user 0 \
+    && npm install -g phantomjs-prebuilt
+RUN apk add --update ttf-dejavu ttf-droid ttf-freefont ttf-liberation ttf-ubuntu-font-family && rm -rf /var/cache/apk/*
