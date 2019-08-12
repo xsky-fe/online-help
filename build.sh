7bde52cbb3a39bb9c1b80a372a0b6701160b545a
yukun <yukun@xsky.com>
2019-08-12 17:56:39 +0800

    fix build

--- build.sh
+++ build.sh
@@ -6,4 +6,3 @@ yarn install
 wget -qO- "https://github.com/dustinblackman/phantomized/releases/download/2.1.1a/dockerized-phantomjs.tar.gz" | tar xz -C / \
     && npm config set user 0 \
     && npm install -g phantomjs-prebuilt
-apk add --update ttf-dejavu ttf-droid ttf-freefont ttf-liberation ttf-ubuntu-font-family && rm -rf /var/cache/apk/*
